9e42f306942d4dcadc2b2cc7dfed5fbe11661abf
ZoePiQian <88176929+ZoePiQian@users.noreply.github.com>
2021-08-20 19:50:04 +0800

    Update Dialog.js

--- src/layout/Dialog.js
+++ src/layout/Dialog.js
@@ -1,11 +1,5 @@
 import React, {Component} from "react";
-import ImageDialog from "../component/Dialog/ImageDialog";
-import LinkDialog from "../component/Dialog/LinkDialog";
-import AboutDialog from "../component/Dialog/AboutDialog";
-import VersionDialog from "../component/Dialog/VersionDialog";
 import FormDialog from "../component/Dialog/FormDialog";
-import HistoryDialog from "../component/Dialog/HistoryDialog";
-import SitDownDialog from "../component/Dialog/SitDownDialog";
 import TutorialDialog from "../component/Dialog/TutorialDialog";
 
 class Dialog extends Component {
